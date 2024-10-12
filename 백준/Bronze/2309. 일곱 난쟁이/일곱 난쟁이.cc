31c454f281603814d148e23d541a66be7c502825
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-12 18:03:11 +0300

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,45 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-int n = 9, k = 7, sum=0;
-vector<int> a;
-
-void print(vector<int> b){
-	for(int i : b) cout << i << " ";
-	cout << "\n";
-}
-
-void combi(int start, vector<int> &b){
-	if (b.size() == k){
-		for (int j = 0; j < k; j++){
-			sum += b[j];
-		}
-		if (sum == 100){
-			print(b);
-			exit(0);
-		}
-		sum = 0;
-		return;
-	}
-	for(int i = start + 1; i < n; i++){
-		b.push_back(a[i]);
-		combi(i, b);
-		b.pop_back();
-	}
-	return;
-}
-
-
+int a[9];
 int main(){
-	for (int i = 0; i < n; i++){
-		int j = 0;
-		cin >> j;
-		if( j>=1 && j <= 100)
-			a.push_back(j);
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    for(int i = 0; i <9; i++){
+    	cin >> a[i];
 	}
-	sort(a.begin(), a.end());
-	vector<int> b;
-	combi(-1, b);
+	sort(a, a+9);
+	do{
+		int sum = 0;
+		for(int i = 0; i < 7; i++)
+		sum += a[i];
+		if (sum==100) break;
+	} while(next_permutation(a, a+9));
+	
+	for(int i= 0; i < 7; i++) cout << a[i] << "\n";
 	
 	return 0;
 }
