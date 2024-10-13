1cb550730530f30fe8883179c7bdfbff7a306ce7
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-13 20:54:16 +0300

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,24 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-int N, K, sum;
+typedef long long ll;
+int n, k, temp, psum[100004], ret = -10000004;
 int main(){
-	cin >> N >> K;
-	int n[N];
-	for(int i = 0; i < N; i++){
-		cin >> n[i];
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+	cin >> n >> k;
+	for(int i = 1; i <= n; i++){
+		cin >> temp; 
+		psum[i] = psum[i - 1] + temp;
 	}
-	for(int j = 0; j < K; j++){
-		sum += n[j];
+	for (int i = k; i <= n; i++){
+		ret = max(ret, psum[i] - psum[i - k]);
 	}
-	for (int i = 1; i < N - K + 1; i++){
-		int temp = n[i];
-		for(int j = i+1; j < i+K; j++){
-			temp += n[j];
-		}
-		if (temp > sum){
-			sum = temp;
-		}
-	}
-	cout << sum;
+	cout << ret << "\n";
+	
 	return 0;
 }
