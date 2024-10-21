4f4b87c62a3e334b0d4e2ce2e1f44437407dfb70
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-21 09:10:16 +0300

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,18 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long a, b, c, ret;
-long long go(long long a, long long b){
+typedef long long ll;
+ll a, b, c;
+ll go(ll a, ll b){
 	if(b == 1) return a % c;
-	ret = go(a, b/2);
+	ll ret = go(a, b /2);
 	ret = (ret * ret) % c;
-	if(b & 1) ret = (ret * a) % c;
+	if(b % 2) ret = (ret * a) % c;
 	return ret;
 }
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);cout.tie(NULL);
 	cin >> a >> b >> c;
-	if(a > INT_MAX || b > INT_MAX || c > INT_MAX) exit(0);
 	cout << go(a,b) << "\n";
 	return 0;
 }
