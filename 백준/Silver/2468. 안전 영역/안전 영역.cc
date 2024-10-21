f42a6af5dfd3066dc8fdf44c3852da542eb0857b
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-21 18:10:40 +0300

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2424 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,41 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int max_n = 104;
-int n, a[max_n][max_n], visited[max_n][max_n], ret, max_height;
-int dy[] ={-1,0,1,0};
-int dx[] ={0,1,0,-1};
-void DFS(int y, int x, int height){
+int ret = 1, cnt, n, a[104][104], visited[104][104];
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+void dfs(int y, int x, int h){
 	visited[y][x] = 1;
 	for(int i = 0; i < 4; i++){
 		int ny = y + dy[i];
 		int nx = x + dx[i];
-		if (ny < 0 || ny >= n || nx < 0 || nx >= n || a[ny][nx] <= height) continue;
-		if(visited[ny][nx] == 1) continue;
-		DFS(ny,nx, height);
+		if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+		if(visited[ny][nx] || a[ny][nx] <= h) continue;
+		dfs(ny, nx, h);
 	}
+	return;
 }
 int main(){
 	cin >> n;
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j < n; j++){
 			cin >> a[i][j];
-			if(max_height < a[i][j]){
-				max_height = a[i][j];
+		}
+	}
+	for(int d = 1; d <= 100; d++){
+		fill(&visited[0][0], &visited[0][0] + 104* 104, 0);
+		cnt = 0;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(visited[i][j] == 0 && a[i][j] > d){
+					dfs(i, j, d);
+					cnt++;
+				}
 			}
 		}
-	}
-	for (int l = 0; l < max_height; l++){
-		fill(&visited[0][0], &visited[n][n], 0);
-		int temp = 0;
-		for (int i = 0; i < n; i++){
-			for (int j = 0; j < n; j++){
-				if(a[i][j] > l && !visited[i][j]){
-					temp++; DFS(i,j, l);
-				}
-			}	
-		}
-		if (temp > ret) ret = temp;
+		ret = max(ret, cnt);
 	}
 	cout << ret;
+
 	return 0;
 }
