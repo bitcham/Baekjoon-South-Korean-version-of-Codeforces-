f2f79c2fc593475dedcd7c0091a90192ebbc4832
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-21 18:47:31 +0300

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,37 +1,35 @@
 #include<bits/stdc++.h>
-using namespace std; 
-typedef long long int ll;     
-int n; 
-string s;   
+using namespace std;
+int n;
 char a[101][101];
+string s;
 string quard(int y, int x, int size){
-    if(size == 1) return string(1, a[y][x]); 
-    char b = a[y][x]; 
-    string ret = "";  
-    for(int i = y; i < y + size; i++){
-        for(int j = x; j < x + size; j++){ 
-            if(b != a[i][j]){ 
-                ret += '(';
-                ret += quard(y, x, size / 2); 
-                ret += quard(y, x + size / 2, size / 2);
-                ret += quard(y + size / 2, x, size / 2);
-                ret += quard(y + size / 2, x + size / 2, size / 2); 
-                ret += ')';
-                return ret;  
-            }
-        } 
-    }
-    return string(1, a[y][x]); 
+	if(size == 1) return string(1, a[y][x]);
+	char b = a[y][x];
+	string ret = "";
+	for (int i = y; i < y + size; i++){
+		for( int j = x; j < x + size; j++){
+			if (b != a[i][j]){
+				ret += '(';
+				ret += quard(y, x, size / 2);
+				ret += quard(y, x + size/ 2, size /2);
+				ret += quard(y + size/2, x, size/ 2);
+				ret += quard(y+size/2, x+size/2,size/2);
+				ret += ')';
+				return ret;
+			}
+		}
+	}
+	return string(1, a[y][x]);
 }
 int main(){
-	ios_base::sync_with_stdio(0); cin.tie(NULL);
-    cin >> n; 
-    for(int i = 0; i < n; i++){
-        cin >> s; 
-        for(int j = 0; j < n; j++){
-            a[i][j] = s[j];
-        }
-    }
-    cout << quard(0, 0, n) << '\n'; 
-	return 0; 
+	cin >> n;
+	for (int i = 0; i < n; i++){
+		cin >> s;
+		for (int j = 0; j < n; j++){
+			a[i][j] = s[j];
+		}
+	}
+	cout << quard(0, 0, n) << '\n';
+	return 0;
 }
