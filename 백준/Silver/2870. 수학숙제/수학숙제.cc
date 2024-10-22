020169cef352524b4f9724c26dfec657403fb5fd
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-22 19:34:38 +0300

    [Silver IV] Title: 수학숙제, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.cc
+++ 백준/Silver/2870. 수학숙제/수학숙제.cc
@@ -1,33 +1,34 @@
-#include <bits/stdc++.h>
-using namespace std;   
-int n; 
-vector<string> v; 
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+vector<string> v;
 string s, ret;
-void go(){   
-	while(true){  
-		if(ret.size() && ret.front() == '0')ret.erase(ret.begin()); 
+void go(){
+	while(true){
+		if(ret.size() && ret.front() == '0')ret.erase(ret.begin());
 		else break;
 	}
-	if(ret.size() == 0) ret = "0"; 
+	if(ret.size() == 0) ret = "0";
 	v.push_back(ret);
-	ret = "";  
+	ret = "";
 }
 bool cmp(string a, string b){
 	if(a.size() == b.size()) return a < b;
 	return a.size() < b.size();
 }
-int main () {
-	cin >> n; 
+int main(){
+	cin >> n;
 	for(int i = 0; i < n; i++){
-		cin >> s; 
-	 	ret = "";
+		cin >> s;
+		ret = "";
 		for(int j = 0; j < s.size(); j++){
-			if(s[j] < 65)ret += s[j];
-			else if(ret.size()) go(); 
+			if(s[j] < 65) ret += s[j];
+			else if(ret.size()) go();
 		}
-		if(ret.size()) go(); 
+		if(ret.size()) go();
 	}
 	sort(v.begin(), v.end(), cmp);
-	for(string i : v)cout << i << "\n"; 
+	for(string i : v)cout << i << "\n";
+	
 	return 0;
 }
