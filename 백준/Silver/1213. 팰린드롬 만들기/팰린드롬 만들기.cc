a1ef67bacdbd0f115d91e637268b7449a77cf3f4
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-26 11:32:04 +0300

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,30 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 string s, ret;
-int cnt[200], flag;
-char mid;
+int flag, a[200], mid;
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
 	cin >> s;
-	for(char a: s)cnt[a]++;
+	for(int i = 0; i < s.size(); i++){
+		a[s[i]]++;
+	}
 	for(int i = 'Z'; i >= 'A'; i--){
-		if (cnt[i]){
-			if(cnt[i] & 1){
-				mid = char(i); flag++;
-				cnt[i]--;
-			}
-			if(flag == 2) break;
-			for (int j = 0; j < cnt[i]; j+=2){
-				ret = char(i) + ret;
-				ret += char(i);
-			}
-			
+		if(a[i] % 2){
+			mid = i;
+			flag++;
+			a[i]--;
+		}
+		if(flag >= 2){
+			break;
+		}
+
+		for(int j = 0; j < a[i]; j += 2){
+			ret = char(i) + ret;
+			ret += char(i);
 		}
 	}
-	if(mid)ret.insert(ret.begin() + ret.size() / 2, mid);
-	if(flag==2)cout << "I'm Sorry Hansoo\n";
-	else cout << ret << "\n";
-	
+	if(mid) ret.insert(ret.size()/2, 1, char(mid));
+	if(flag >= 2) cout << "I'm Sorry Hansoo" << "\n";
+	else cout << ret;
 	return 0;
 }
