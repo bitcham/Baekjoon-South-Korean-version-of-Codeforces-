2d6576f701de8fbf363b44183fa0391c50545c0a
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-28 16:15:57 +0200

    [Silver I] Title: 효율적인 해킹, Time: 2608 ms, Memory: 3540 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -3,17 +3,16 @@ using namespace std;
 vector<int> v[10001];
 int dp[10001], mx, visited[10001], n, m, a, b;
 int dfs(int here){
-	int ret = 1;
 	visited[here] = 1;
-	for(int there : v[here]){
+	int ret = 1;
+	for(int there: v[here]){
 		if(visited[there]) continue;
 		ret += dfs(there);
 	}
 	return ret;
 }
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);cout.tie(NULL);
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> m;
 	while(m--){
 		cin >> a >> b;
