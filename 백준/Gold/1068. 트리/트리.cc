8529d9f026ad24e0108a08ed0016bc21ca6eebe7
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-28 15:50:32 +0200

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, r, temp, root;
-vector<int> adj[54];
+int n, m, temp, root;
+vector<int> a[51];
 int dfs(int here){
 	int ret = 0;
 	int child = 0;
-	for(int there : adj[here]){
-		if(there == r) continue;
+	for(auto there : a[here]){
+		if(there == m) continue;
 		ret += dfs(there);
 		child++;
 	}
@@ -14,18 +14,17 @@ int dfs(int here){
 	return ret;
 }
 int main(){
-	ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
 	cin >> n;
 	for(int i = 0; i < n; i++){
 		cin >> temp;
 		if(temp == -1) root = i;
-		else adj[temp].push_back(i);
+		else a[temp].push_back(i);
 	}
-	cin >> r;
-	if(r == root){
-		cout << 0 << "\n";
+	cin >> m;
+	if(m == root) {
+		cout << 0;
 		return 0;
 	}
-	cout << dfs(root) << "\n";
+	else cout << dfs(root);
 	return 0;
 }
