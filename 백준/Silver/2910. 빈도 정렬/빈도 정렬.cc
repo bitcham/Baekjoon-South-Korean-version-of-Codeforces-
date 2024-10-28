e31b73bdad769e368a75956ea6bc072f8084355d
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-28 15:26:59 +0200

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,27 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, c, a[1004];
-vector<pair<int, int>> v;
-map<int, int> mp, mp_first;
+int n, c, temp;
+unordered_map <int, int> mp1, mp2;
 bool cmp(pair<int, int> a, pair<int, int> b){
-	if(a.first == b.first){
-		return mp_first[a.second] < mp_first[b.second];
-	}
-	return a.first > b.first;
+	if(a.second == b.second) return mp2[a.first] < mp2[b.first];
+	return a.second > b.second;
 }
 int main(){
 	cin >> n >> c;
 	for(int i = 0; i < n; i++){
-		cin >> a[i]; mp[a[i]]++;
-		if(mp_first[a[i]] == 0) mp_first[a[i]] = i + 1;
+		cin >> temp;
+		mp1[temp]++;
+		if(mp2.find(temp) == mp2.end()) {  
+            mp2[temp] = i;
+        }
 	}
-	for(auto it : mp){
-		v.push_back({it.second, it.first});
-	}
+	vector<pair<int, int>> v(mp1.begin(), mp1.end());
 	sort(v.begin(), v.end(), cmp);
-	for(auto i : v){
-		for(int j = 0; j < i.first; j++){
-			cout << i.second << " ";
+	for(auto it : v){
+		for(int i = 0; i < it.second; i++){
+			cout << it.first << " ";
 		}
 	}
+	return 0;
 }
