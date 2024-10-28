d05b6b508a59b77e3cac61a74159e1be5d561694
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-28 11:19:56 +0200

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,35 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n;
-string s;
-char a[101][101];
-string quard(int y, int x, int size){
-	if(size == 1) return string(1, a[y][x]);
-	char b = a[y][x];
-	string ret ="";
-	for(int i = y; i < y + size; i++){
+string s[70];
+string go(int y, int x, int size){
+	if(size == 1) return string(1, s[y][x]);
+	string ret = "";
+	char b = s[y][x];
+	for(int i = y; i < y+size; i++){
 		for(int j = x; j < x+size; j++){
-			if(b != a[i][j]){
+			if(s[i][j] != b){
 				ret += '(';
-				ret += quard(y, x, size/2);
-				ret += quard(y, x +size /2, size /2);
-				ret += quard(y+size/2, x, size/2);
-				ret += quard(y + size / 2, x + size / 2, size/2);
+				ret += go(y, x, size/2);
+				ret += go(y, x + size /2, size /2);
+				ret += go(y+size/2, x, size/2);
+				ret += go(y+size/2, x+size/2, size/2);
 				ret += ')';
 				return ret;
 			}
 		}
 	}
-	return string(1, a[y][x]);
+	return string(1, b);
+	
 }
 int main(){
 	cin >> n;
 	for(int i = 0; i < n; i++){
-		cin >> s;
-		for (int j = 0; j < n; j++){
-			a[i][j] = s[j];
-		}
-	} 
-	cout << quard(0, 0, n) <<'\n';
+		cin >> s[i];
+	}
+	cout << go(0, 0, n);
 	return 0;
 }
