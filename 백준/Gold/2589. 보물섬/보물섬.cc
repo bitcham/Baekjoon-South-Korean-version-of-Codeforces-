93d52191e65e3854f865394d18713a7928d5853b
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-29 09:46:37 +0200

    [Gold V] Title: 보물섬, Time: 128 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,40 +1,42 @@
 #include<bits/stdc++.h>
-using namespace std; 
-int n, m, mx, visited[54][54]; 
-const int dy[] = {-1, 0, 1, 0}; 
-const int dx[] = {0, 1, 0, -1}; 
-char a[54][54];
-void bfs(int y, int x){
-    memset(visited, 0, sizeof(visited)); 
-    visited[y][x] = 1; 
-    queue<pair<int, int>> q; 
-    q.push({y, x}); 
-    while(q.size()){
-        tie(y, x) = q.front(); q.pop(); 
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i]; 
-            int nx = x + dx[i]; 
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue; 
-            if(visited[ny][nx]) continue; 
-            if(a[ny][nx] == 'W')continue;
-            visited[ny][nx] = visited[y][x] + 1; 
-            q.push({ny, nx});
-            mx = max(mx, visited[ny][nx]);
-        }
-    }
-    return;
+using namespace std;
+int n, m, ret, visited[51][51];
+char a[51][51];
+int dy[] = {-1, 0, 1, 0}, dx[] = {0, -1, 0, 1};
+int bfs(int y, int x){
+	visited[y][x] = 1;
+	int mx = 0;
+	queue<pair<int, int>> q;
+	q.push({y, x});
+	while(q.size()){
+		tie(y, x) = q.front(); q.pop();
+		for(int i = 0; i < 4; i++){
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if(ny < 0 || ny >= n || nx < 0 || nx >= m)continue;
+			if(visited[ny][nx] || a[ny][nx] != 'L')continue;
+			visited[ny][nx] = visited[y][x] + 1;
+			q.push({ny, nx});
+			mx = max(mx, visited[ny][nx]);
+		}
+	}
+	return mx - 1;
 }
 int main(){
-    cin >> n >> m; 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            cin >> a[i][j]; 
-        }
-    }
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(a[i][j] == 'L') bfs(i, j); 
-        }
-    } 
-    cout << mx - 1 << "\n";
+	cin >> n >> m;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			cin >> a[i][j];
+		}
+	}
+	for(int i = 0; i < n; i++){
+		for(int j = 0 ; j < m; j++){
+			memset(visited, 0, sizeof(visited));
+			if(visited[i][j] == 0 && a[i][j] == 'L'){
+				ret = max(ret, bfs(i, j));
+			}
+		}
+	}
+	cout << ret;
+	return 0;
 }
