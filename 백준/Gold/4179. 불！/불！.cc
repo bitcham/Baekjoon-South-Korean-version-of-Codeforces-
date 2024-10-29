c252e81be3963e94d2d9a1a6f76b4886f1ce28cf
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-29 11:11:56 +0200

    [Gold III] Title: 불!, Time: 44 ms, Memory: 10964 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,63 +1,58 @@
-#include <bits/stdc++.h> 
+#include<bits/stdc++.h>
 using namespace std;
 const int INF = 987654321;
-char a[1004][1004];
-int n,m,sx,sy,dx[4]={-1,0,1,0},dy[4]={0,-1,0,1}, ret, y, x;
-int fire_check[1004][1004], person_check[1004][1004];
-bool in(int a,int b){
-	return 0 <= a && a < n && 0 <= b && b < m;
-}
+int r, c, personCheck[1001][1001], fireCheck[1001][1001], sy, sx, y, x, ret;
+char a[1001][1001];
+int dy[] = {-1, 0, 1, 0}, dx[] = {0, -1, 0, 1};
 int main(){
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0); 
 	queue<pair<int, int>> q;
-    cin >> n >> m;  
-	fill(&fire_check[0][0], &fire_check[0][0] + 1004 * 1004, INF);
-    for(int i = 0; i < n; i++){ 
-        for(int j = 0; j < m; j++){
-            cin >> a[i][j];
-            if(a[i][j] == 'F'){
-                fire_check[i][j] = 1; q.push({i, j});
-            }else if(a[i][j] == 'J'){
-                sy = i; sx = j;
-            }
-        }
-    } 
-
+	cin >> r >> c;
+	fill(&fireCheck[0][0], &fireCheck[0][0] + 1001 * 1001, INF);
+	for(int i = 0; i < r; i++){
+		for(int j = 0; j < c; j++){
+			cin >> a[i][j];
+			if(a[i][j] == 'F') {
+				fireCheck[i][j] = 1;
+				q.push({i, j});
+			}
+			if(a[i][j] == 'J'){
+				sy = i; sx = j;
+			}
+		}
+	}
+	
 	while(q.size()){
-        tie(y, x) = q.front(); 
-		q.pop();
+		tie(y,x) = q.front();q.pop();
 		for(int i = 0; i < 4; i++){
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if(!in(ny,nx)) continue;
-			if(fire_check[ny][nx] != INF || a[ny][nx]=='#') continue;
-			fire_check[ny][nx] = fire_check[y][x] + 1;
+			if(ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+			if(fireCheck[ny][nx] != INF || a[ny][nx] == '#') continue;
+			fireCheck[ny][nx] = fireCheck[y][x] + 1;
 			q.push({ny, nx});
 		}
 	}
-
-	person_check[sy][sx]=1;
-	q.push({sy,sx});
+	q.push({sy, sx});
+	personCheck[sy][sx] = 1;
 	while(q.size()){
-		int y = q.front().first;
-		int x = q.front().second;
-		q.pop(); 
-		if(x == m - 1 || y == n - 1 || x == 0 || y == 0){
-            ret = person_check[y][x];
-            break;
+		tie(y, x) = q.front(); q.pop();
+		if(y == 0 || x == 0 || y == r -1 || x == c -1){
+			ret = personCheck[y][x];
+			break;
 		}
 		for(int i = 0; i < 4; i++){
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if(!in(ny,nx)) continue;
-			if(person_check[ny][nx] || a[ny][nx]=='#') continue; 
-			if(fire_check[ny][nx] <= person_check[y][x] + 1) continue;
-            person_check[ny][nx] = person_check[y][x] + 1;
-            q.push({ny, nx});
+			if(ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+			if(fireCheck[ny][nx] <= personCheck[y][x] + 1) continue;
+			if(personCheck[ny][nx] != 0 || a[ny][nx] == '#') continue;
+			personCheck[ny][nx] = personCheck[y][x] + 1;
+			q.push({ny, nx});
 		}
-	} 
-    if(ret != 0) cout << ret << "\n";
-	else cout << "IMPOSSIBLE \n";
+	}
+	if(ret != 0) cout << ret;
+	else cout << "IMPOSSIBLE";
 	return 0;
 }
