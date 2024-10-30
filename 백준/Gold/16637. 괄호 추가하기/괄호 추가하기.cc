bb7650b6624912b35cf5c8c7e19a4bf6e4e1dda1
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-30 20:12:39 +0200

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -1,37 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> _num;
-vector<char> _oper;
-string s;
+vector<int> num;
+vector<char> operStr;
 int n, ret = -987654321;
+string s;
 int oper(char a, int b, int c){
 	if(a == '+') return b + c;
 	if(a == '-') return b - c;
 	if(a == '*') return b * c;
 }
-void go(int here, int num){
-	if(here == _num.size() - 1){
-		ret = max(ret, num);
+void go(int here, int _num){
+	if(here == num.size() - 1){
+		ret = max(ret, _num);
 		return;
 	}
-	
-	go(here + 1, oper(_oper[here], num, _num[here + 1]));
-	if(here + 2 <= _num.size() - 1){
-		int temp = oper(_oper[here + 1], _num[here + 1], _num[here + 2]);
-		go(here + 2, oper(_oper[here], num, temp));
+	go(here + 1, oper(operStr[here], _num, num[here + 1]));
+	if(here + 2 <= num.size() - 1){
+		int temp = oper(operStr[here + 1], num[here + 1], num[here + 2]);
+		go(here + 2, oper(operStr[here], _num, temp));
 	}
 	return;
 }
 int main(){
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
-    cout.tie(NULL); 
 	cin >> n >> s;
 	for(int i = 0; i < n; i++){
-		if(i % 2 == 0) _num.push_back(s[i] - '0');
-		else _oper.push_back(s[i]);
+		if(i % 2 == 0) num.push_back(s[i] - '0');
+		else operStr.push_back(s[i]);
 	}
-	go(0, _num[0]);
-	cout << ret << "\n";
+	go(0, num[0]);
+	cout << ret;
 	return 0;
 }
