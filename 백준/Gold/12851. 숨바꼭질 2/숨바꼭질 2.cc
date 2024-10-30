c99960b17823f34b68864dbc2b709bfb6cecea3a
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-10-30 20:39:10 +0200

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,33 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int MAX = 100000;
-int visited[MAX + 1], cnt[MAX + 1], n, m;
+int cnt[MAX + 1], visited[MAX + 1], n, m;
 int main(){
 	cin >> n >> m;
 	if(n == m){
 		puts("0"); puts("1");
 		return 0;
 	}
-	visited[n] = 1;
-	cnt[n] = 1;
 	queue<int> q;
+	visited[n] = 1;
 	q.push(n);
+	cnt[n]++;
 	while(q.size()){
-		int now = q.front();
-		q.pop();
-		for(int next : {now - 1, now + 1, now * 2}){
-			if( 0 <= next && next <= MAX){
-				if(visited[next] == 0){
-					q.push(next);
-					visited[next] = visited[now] + 1;
-					cnt[next] += cnt[now];
-				} else if(visited[next] == visited[now] + 1){
-				cnt[next] += cnt[now];
+		int x = q.front(); q.pop();
+		for(int num : {x + 1, x - 1, x * 2}){
+			if(num >= 0 && num <= MAX){
+				if(!visited[num]){
+					cnt[num] += cnt[x];
+					visited[num] = visited[x] + 1;
+					q.push(num);
+				}else if(visited[num] == visited[x] + 1){
+					cnt[num] += cnt[x];
 				}
 			}
 		}
 	}
-	cout << visited[m] - 1 << '\n';
-	cout << cnt[m] << '\n';
+	cout << visited[m] - 1 << "\n" << cnt[m];
 	return 0;
 }
