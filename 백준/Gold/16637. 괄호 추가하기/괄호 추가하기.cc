9dc8795b91bca8770ba97839cf101b4d5891dd71
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-11-01 13:35:30 +0200

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -1,33 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+int ret = -987654321;
+int n;
+char s[20];
 vector<int> num;
-vector<char> operStr;
-int n, ret = -987654321;
-string s;
+vector<char> _oper;
 int oper(char a, int b, int c){
 	if(a == '+') return b + c;
 	if(a == '-') return b - c;
 	if(a == '*') return b * c;
 }
-void go(int here, int _num){
+void solve(int here, int _num){
 	if(here == num.size() - 1){
-		ret = max(ret, _num);
-		return;
+		ret = max(ret, _num); return;
 	}
-	go(here + 1, oper(operStr[here], _num, num[here + 1]));
-	if(here + 2 <= num.size() - 1){
-		int temp = oper(operStr[here + 1], num[here + 1], num[here + 2]);
-		go(here + 2, oper(operStr[here], _num, temp));
+	solve(here + 1, oper(_oper[here], _num, num[here + 1]));
+	if(here + 2 <= num.size() -1){
+		int temp = oper(_oper[here + 1], num[here + 1], num[here + 2]);
+		solve(here + 2, oper(_oper[here], _num, temp));
 	}
 	return;
 }
 int main(){
-	cin >> n >> s;
+	cin >> n;
 	for(int i = 0; i < n; i++){
-		if(i % 2 == 0) num.push_back(s[i] - '0');
-		else operStr.push_back(s[i]);
+		cin >> s[i];
+		if(s[i] == '+' || s[i] == '-' || s[i] == '*') _oper.push_back(s[i]);
+		else num.push_back(s[i] - '0');
 	}
-	go(0, num[0]);
+	solve(0, num[0]);
 	cout << ret;
 	return 0;
 }
