c75e4d283f7789c7a0f185b10c7a78354f412e8d
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-11-04 20:29:23 +0200

    [Silver II] Title: 꽃길, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.cc
+++ 백준/Silver/14620. 꽃길/꽃길.cc
@@ -1,61 +1,60 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int n, ret=987654321, v[20][20], p[20][20], dy[4] = { -1,1,0,0 }, dx[4] = {0,0,-1,1};
+int n, p[11][11], v[11][11], ret = 987654321;
+int dy[] = {-1, 0, 1, 0}, dx[] = {0, -1, 0, 1};
 
-int setFlower(int y, int x){ 
-    v[y][x] = 1;
-    int s = p[y][x];
-    for (int i = 0; i < 4; i++) {
-        int ny = y + dy[i];
+void goErase(int y, int x){
+	v[y][x] = 0;
+	for(int i = 0; i < 4; i++){
+		int ny = y + dy[i];
         int nx = x + dx[i];
-        v[ny][nx] = 1;
-        s += p[ny][nx];
-    }
-    return s; 
+        v[ny][nx] = 0;
+	}
+	return;
 }
-bool check(int y, int x) { 
-    if (v[y][x]) return 0;
-    for (int i = 0; i < 4; i++) {
-        int ny = y + dy[i];
+bool check(int y, int x){
+	if(v[y][x]) return 0;
+	for(int i = 0; i < 4; i++){
+		int ny = y + dy[i];
         int nx = x + dx[i];
         if (ny<0 || ny>=n || nx < 0 || nx >= n || v[ny][nx]) {
             return 0;
-        } 
-    } 
-    return 1;  
+        }
+	}
+	return 1;
 }
-
-void eraseFlower(int y, int x) {
-    v[y][x] = 0;
-    for (int i = 0; i < 4; i++) {
-        int ny = y + dy[i];
+int setFlower(int y, int x){
+	v[y][x] = 1;
+	int s = p[y][x];
+	for(int i = 0; i < 4; i++){
+		int ny = y + dy[i];
         int nx = x + dx[i];
-        v[ny][nx] = 0;
-    }
+        v[ny][nx] = 1;
+        s += p[ny][nx];
+	}
+	return s;
 }
-void flower(int cnt,int hap) {
-    if (cnt == 3) {
-        ret = min(ret, hap);
-        return;
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) { 
-            if (check(i, j)) {   
-                flower(cnt+1,hap + setFlower(i, j)); 
-                eraseFlower(i, j);
-            }
-        }
-    } 
+void go(int cnt, int money){
+	if(cnt == 3) {
+		ret = min(ret, money);return;
+	}
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(check(i, j)){
+				go(cnt + 1, money + setFlower(i, j));
+				goErase(i, j);
+			}
+		}
+	}
 }
-
-int main()
-{
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> p[i][j];
-        }
-    }
-    flower(0, 0);
-    cout << ret;
+int main(){
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cin >> p[i][j];
+		}
+	}
+	go(0, 0);
+	cout << ret;
+	return 0;
 }
