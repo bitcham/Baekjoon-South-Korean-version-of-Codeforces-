cdea16c5cda9ffbd4df004e1eb9aec0a79e681cf
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-11-05 23:00:29 +0200

    [Gold IV] Title: 알파벳, Time: 412 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,33 +1,36 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int R, C, ret, ny, nx, visited[30];
+int r, c, ret = -987654321, cnt[30], visited[21][21];
 char a[21][21];
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = { 0, 1, 0, -1}; 
-void go(int y, int x, int cnt){
-    ret = max(ret, cnt);
-    for(int i = 0; i < 4; i++){
-        ny = y + dy[i], nx = x + dx[i];
-        if(ny < 0 || ny >= R || nx < 0 || nx >= C) continue;
-        int next = (int)(a[ny][nx] - 'A');
-        
-        if(visited[next] == 0){
-            visited[next] = 1; 
-            go(ny, nx, cnt + 1);
-            visited[next] = 0;  
-        } 
-    }
-    return;
+string s;
+int dy[] = { -1, 0, 1, 0}, dx[] = { 0, -1, 0, 1};
+void solve(int y, int x){
+	ret = max(ret, visited[y][x]);
+	for(int i = 0; i < 4; i++){
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if(ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+		if(visited[ny][nx]) continue;
+		if(cnt[a[ny][nx] - 'A']) continue;
+		cnt[a[ny][nx] - 'A']++;
+		visited[ny][nx] = visited[y][x] + 1;
+		solve(ny, nx);
+		cnt[a[ny][nx] - 'A'] = 0;
+		visited[ny][nx] = 0;
+	}
+	return;
 }
 int main(){
-    cin >> R >> C;
-    for(int i = 0; i < R; i++){
-        for(int j = 0; j < C; j++){
-            cin >> a[i][j];
-        }
-    }
-    visited[(int)a[0][0] - 'A'] = 1;
-    go(0, 0, 1);
-    cout << ret << '\n';
-    return 0;
+	cin >> r >> c;
+	for(int i = 0; i < r; i++){
+		cin >> s;
+		for(int j = 0; j < c; j++){
+			a[i][j] = s[j];
+		}
+	}
+	visited[0][0] = 1;
+	cnt[a[0][0] - 'A'] = 1;
+	solve(0, 0);
+	cout << ret;
+	return 0;
 }
