6dba16f8e763861198b14e40b59ff8fe80ccb020
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-11-10 21:50:37 +0200

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -1,29 +1,33 @@
 #include<bits/stdc++.h>
-using namespace std; 
-int n, l, a[104][104], b[104][104], ret; 
-void solve(int a[104][104]){
-    for(int i = 0; i < n; i++){
-        int cnt = 1; 
-        int j;  
-        for(j = 0; j < n - 1; j++){
-            if(a[i][j] == a[i][j + 1])cnt++; 
-            else if(a[i][j] + 1 == a[i][j + 1] && cnt >= l) cnt = 1; 
-            else if(a[i][j] - 1 == a[i][j + 1] && cnt >= 0) cnt = -l + 1; 
-            else break;
-        }
-        if(j == n - 1 && cnt >= 0) ret++; 
-    }
-    return; 
-} 
+using namespace std;
+int n, l, a[101][101], b[101][101];
+int solve(int c[101][101]){
+	int ret = 0;
+	for(int i = 0; i < n; i++){
+		int cnt = 1;
+		int j;
+		for(j = 0; j < n - 1; j++){
+			if(c[i][j] == c[i][j+1]) cnt++;
+			else if(c[i][j] + 1 == c[i][j + 1] && cnt >= l) cnt = 1;
+			else if(c[i][j] - 1 == c[i][j + 1] && cnt >= 0){
+				cnt = -l + 1;
+			}
+			else {
+				break;
+			}
+		}
+		if(j == n - 1 && cnt >= 0) ret++;
+	}
+	return ret;
+}
 int main(){
-    scanf("%d %d", &n, &l);
-    for(int i = 0; i < n; i++){ 
-        for(int j = 0; j < n; j++){
-            scanf("%d", &a[i][j]); 
-            b[j][i] = a[i][j];
-        }
-    }    
-    solve(a); solve(b);  
-    printf("%d\n", ret);  
-    return 0; 
+	cin >> n >> l;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cin >> a[i][j];
+			b[j][i] = a[i][j];
+		}
+	}
+	cout << solve(a) + solve(b);
+	return 0;
 }
