91c64fee9f5194cc6652be80d2930dbd68c06374
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2024-11-13 18:07:37 +0200

    [Gold III] Title: 성곽, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/2234. 성곽/성곽.cc
+++ 백준/Gold/2234. 성곽/성곽.cc
@@ -1,55 +1,56 @@
-#include <bits/stdc++.h>
-using namespace std;   
-const int dy[] = {0, -1, 0, 1}; 
-const int dx[] = {-1, 0, 1, 0}; 
-int visited[51][51], a[51][51], cnt, compSize[2504], n, m, mx, big; 
-int dfs(int y, int x, int cnt){
-    if(visited[y][x]) return 0; 
-    visited[y][x] = cnt; 
-    int ret = 1; 
-    for(int i = 0; i < 4; i++){
-        if(!(a[y][x] & (1 << i))){
-            int ny = y + dy[i]; 
-            int nx = x + dx[i]; 
-            ret += dfs(ny, nx, cnt); 
-        } 
-    } 
-    return ret; 
+#include<bits/stdc++.h>
+using namespace std;
+int c, r, a[51][51], visited[51][51], cnt, ret, ret_m[2504], maxN;
+const int dy[] = {0, -1, 0, 1}, dx[] = {-1, 0, 1, 0};
+int dfs(int y, int x, int value){
+	visited[y][x] = value;
+	int ret = 1;
+	for(int i = 0; i < 4; i++){
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if(ny < 0 || ny >= r || nx < 0 || nx >= c)continue;
+		if(visited[ny][nx])continue;
+		if(a[y][x] & (1 << i)) continue;
+		ret += dfs(ny, nx, value);
+	}
+	return ret;
 }
 int main(){
-    cin >> n >> m;
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            cin >> a[i][j]; 
-        }
-    }
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            if(!visited[i][j]){
-                cnt++; 
-                compSize[cnt] = dfs(i, j, cnt);  
-                mx = max(mx, compSize[cnt]);
-            } 
-        }
-    }
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            if(i + 1 < m){
+	cin >> c >> r;
+	for(int i = 0; i < r; i++){
+		for(int j = 0; j < c; j++){
+			cin >> a[i][j];
+		}
+	}
+	for(int i = 0; i < r; i++){
+		for(int j = 0; j < c; j++){
+			if(!visited[i][j]){
+				cnt++;
+				ret_m[cnt] = dfs(i, j, cnt);
+				ret = max(ret, ret_m[cnt]);
+			}
+		}
+	}
+	
+	for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            if(i + 1 < r){
                 int a = visited[i + 1][j]; 
                 int b = visited[i][j]; 
                 if(a != b){ 
-                    big = max(big, compSize[a] + compSize[b]); 
+                    maxN = max(maxN, ret_m[a] + ret_m[b]); 
                 }
             }
-            if(j + 1 < n){
+            if(j + 1 < c){
                 int a = visited[i][j + 1]; 
                 int b = visited[i][j]; 
                 if(a != b){ 
-                    big = max(big, compSize[a] + compSize[b]); 
+                    maxN = max(maxN, ret_m[a] + ret_m[b]);  
                 }
             } 
         } 
-    } 
-    cout << cnt << "\n" << mx << "\n" << big <<'\n'; 
-    return 0;
+    }
+    cout << cnt << "\n" << ret << "\n";
+	cout << maxN;
+	return 0;
 }
